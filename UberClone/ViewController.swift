8b85ac81cfdc098dcb24d1c1c39a63bb87d147e9
canakyildz <canakyildz53@gmail.com>
2021-05-12 12:32:09 +0300

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Apple on 12.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UberCloneTests/UberCloneTests.swift
@@ -0,0 +1,33 @@
+//
+//  UberCloneTests.swift
+//  UberCloneTests
+//
+//  Created by Apple on 12.05.2021.
+//
+
+import XCTest
+@testable import UberClone
+
+class UberCloneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
